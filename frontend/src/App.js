6e9d31c04bc059d008e94fc0bb4c9ddbd7adc58a
nitesh <niteshpandit676@gmail.com>
2022-06-15 14:22:03 +0530

    parrent folder removed & try to maintian folder structure

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginAndSignUp from './component/LoginSignUp/LoginAndSignUp';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginAndSignUp />
+    </div>
+  );
+}
+
+export default App;
