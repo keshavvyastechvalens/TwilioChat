7278009fb812902f0f93ba61c420d3b4b5750fe7
Keshavvyas588 <keshav.vyas@techvalens.com>
2022-06-15 13:52:55 +0530

    pm.xml changes

--- backend/src/main/java/com/chat/TwilioChat/filter/CROSfilter.java
+++ backend/src/main/java/com/chat/TwilioChat/filter/CROSfilter.java
@@ -1,20 +1,11 @@
 package com.chat.TwilioChat.filter;
 import java.io.IOException;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-
-import io.jsonwebtoken.Jwts;
 
 @Component
 public class CROSfilter implements Filter{
@@ -22,7 +13,6 @@ public class CROSfilter implements Filter{
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		// TODO Auto-generated method stub
 		
 	}
 	
@@ -73,6 +63,7 @@ public class CROSfilter implements Filter{
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 	}*/
+
 }
 
 
