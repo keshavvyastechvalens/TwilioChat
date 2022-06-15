45f4f8200186d2dcd45aba9a32e02716f4bf1b79
Keshavvyas588 <keshav.vyas@techvalens.com>
2022-06-15 12:02:50 +0530

    folder sturucture changed

---
+++ backend/src/main/java/com/chat/TwilioChat/TwilioChatApplication.java
@@ -0,0 +1,13 @@
+package com.chat.TwilioChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwilioChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwilioChatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/chat/TwilioChat/TwilioChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.TwilioChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwilioChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
