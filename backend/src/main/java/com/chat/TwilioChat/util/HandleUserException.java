28583876a1d086b979d45535ac1ba1169f93ff0f
Bishwajeet Samal <bishwajeetsamal10@gmail.com>
2022-06-15 12:56:51 +0530

    exception file in util add

---
+++ backend/src/main/java/com/chat/TwilioChat/util/HandleUserException.java
@@ -0,0 +1,28 @@
+package com.demo.todo.util;
+
+public class HandleUserException extends Exception {
+	public HandleUserException() {
+		super();
+	}
+
+	public HandleUserException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+
+	}
+
+	public HandleUserException(String message, Throwable cause) {
+		super(message, cause);
+
+	}
+
+	public HandleUserException(String message) {
+		super(message);
+
+	}
+
+	public HandleUserException(Throwable cause) {
+		super(cause);
+
+	}
+}
