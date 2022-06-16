f9d0fba5a191e7ca8d54c26074f13109835964a6
Bishwajeet Samal <bishwajeetsamal10@gmail.com>
2022-06-16 18:20:37 +0530

    create conversation and  twillio credential add

--- backend/src/main/java/com/chat/TwilioChat/controller/SoloChatController.java
+++ backend/src/main/java/com/chat/TwilioChat/controller/SoloChatController.java
@@ -2,6 +2,7 @@ package com.chat.TwilioChat.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,15 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.chat.TwilioChat.response.DataResponse;
 import com.chat.TwilioChat.response.RestResponse;
 import com.chat.TwilioChat.services.SoloChatService;
 
 @RestController
 @RequestMapping("/chat")
 public class SoloChatController {
-	
+	@Autowired
 	SoloChatService soloChatService;
-	
 	
 	
 	
@@ -31,8 +32,7 @@ public class SoloChatController {
 		}
 		catch(Exception e)
 		{
-			System.out.println(e);
+			return new DataResponse(500, e.getMessage(), null);
 		}
-		return null;
 	}
 }
--- backend/src/main/java/com/chat/TwilioChat/servicesImpl/SoloChatServiceImpl.java
+++ backend/src/main/java/com/chat/TwilioChat/servicesImpl/SoloChatServiceImpl.java
@@ -3,6 +3,7 @@ package com.chat.TwilioChat.servicesImpl;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.chat.TwilioChat.model.SoloChat;
@@ -12,6 +13,7 @@ import com.chat.TwilioChat.repository.UsersRepository;
 import com.chat.TwilioChat.response.DataResponse;
 import com.chat.TwilioChat.response.RestResponse;
 import com.chat.TwilioChat.services.SoloChatService;
+import com.twilio.Twilio;
 import com.twilio.rest.conversations.v1.Conversation;
 import com.twilio.rest.conversations.v1.conversation.Participant;
 
@@ -24,32 +26,42 @@ public class SoloChatServiceImpl implements SoloChatService {
 	@Autowired
 	SoloChatRepository soloChatRepository;
 
+	@Value("${ACCOUNT_SID}")
+private String acc_sid;
+
+	@Value("${AUTH_TOKEN}")
+	private String auth_token;
+
 	@Override
 	public RestResponse createConversation(long senderUserId, long receiverUserId) {
-
+		Twilio.init(acc_sid,auth_token);
 		Optional<Users> senderCheck = usersRepository.findById(senderUserId);
-		Optional<Users> receiverCheck = usersRepository.findById(senderUserId);
+		Optional<Users> receiverCheck = usersRepository.findById(receiverUserId);
 		Users sender = senderCheck.get();
-		Users receiver = receiverCheck.get();
 		
+		Users receiver = receiverCheck.get();
+		System.out.println(sender);
+		System.out.println(receiver);
 		
 		//here condition for alredy existing convo should be given
 		
 		String convoName = sender.getFirstName()+"-"+receiver.getFirstName();
-		
+		System.out.println("1");
 		Conversation conversation = Conversation.creator().setFriendlyName(convoName).create();
-		
+		System.out.println("2");
 		Participant senderParticipant = Participant.creator(conversation.getSid()).setIdentity(sender.getUserName())
 				.create();
-		
+				System.out.println("3");
 		Participant receiverParticipant = Participant.creator(conversation.getSid()).setIdentity(receiver.getUserName())
 				.create();
-		
+				System.out.println("4");
 		SoloChat soloChat = new SoloChat();
-		
+		System.out.println("5");
 		soloChat.setSender(sender);
+		System.out.println("6");
 		soloChat.setReceiver(receiver);
 		soloChat.setConversationId(conversation.getSid());
+		System.out.println(soloChat);
 		soloChat.setSenderParticipantId(senderParticipant.getSid());
 		soloChat.setReceiverParticipantId(receiverParticipant.getSid());
 		SoloChat returnSoloChatObj = soloChatRepository.save(soloChat);
